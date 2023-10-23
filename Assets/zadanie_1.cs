9ef7923f8c7a604383e7b7387100e831a98658cc
Tomasz_Piotrowski_162556 <162556@student.uwm.edu.pl>
2023-10-23 19:53:01 +0200

    zadanie_1

---
+++ Assets/zadanie_1.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+public class zadanie_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1f;
+    private bool Forward = true;
+    void Start()
+    {
+       transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Forward)
+        {
+            if(transform.position.x > -10f)
+            {
+                transform.position += new Vector3(-10f * Time.deltaTime * speed,0,0);
+            }
+            else
+            {
+                Forward = false;
+            }
+        }
+        else
+        {
+            if(transform.position.x < 0f)
+            {
+                transform.position += new Vector3(10f * Time.deltaTime * speed,0,0);
+            }
+            else
+            {
+                Forward = true;
+            }
+
+            
+        }
+        
+
+       
+         
+        
+        
+
+         
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity
+# Tomasz Piotrowski
